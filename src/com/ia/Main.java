e2c031084970500366b804962a0def42ee788c43
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-16 11:50:45 +0200

    Tema Madalin

--- src/com/ia/Main.java
+++ src/com/ia/Main.java
@@ -3,7 +3,7 @@ package com.ia;
 public class Main {
 
     public static void main(String[] args) {
-        int n=4;
+        int n=10;
         int i, j, k;
 	for (i = 0; i < n; i++){
 	    for (j = n-i-1;j > 0; j--){
